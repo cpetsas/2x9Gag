9651ec460f2ef137edc757927fa2706cc9b6e584
harry <charis.petsas96@gmail.com>
2022-04-18 12:39:40 +0100

    removed some comments

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,14 +3,6 @@ const router = express.Router()
 const postsController = require('../controllers/post_controller')
 const upload = require('../uploads_config')
 
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, './uploads')
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.originalname)
-//     }
-// })
 
 router.post('/:id', upload.single('photo'), postsController.attachImage)
 router.post('/', postsController.createPost)
